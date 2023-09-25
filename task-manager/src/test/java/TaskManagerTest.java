44e19b800582e75e8d91c6752af3fcc1d7c997f1
Yovany Cunha <yovany33@gmail.com>
2023-09-25 11:01:22 -0300

    update junit

--- task-manager/src/test/java/TaskManagerTest.java
+++ task-manager/src/test/java/TaskManagerTest.java
@@ -2,69 +2,83 @@ package test.java;
 
 import main.java.org.com.TaskManager;
 import main.java.org.com.model.Priority;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.*;
 
 import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TaskManagerTest {
     private TaskManager taskManager;
 
-    @Before
-    public void setUp() {
-
+    @BeforeEach
+    public  void setUp() {
         taskManager = new TaskManager();
         taskManager.createTask("Test Task 1", "Task Description 1", LocalDate.parse("2023-08-31"), Priority.ALTA);
         taskManager.createTask("Test Task 3", "Task Description 3", LocalDate.parse("2023-09-15"), Priority.BAIXA);
         taskManager.createTask("Test Task 2", "Task Description 2", LocalDate.parse("2023-08-29"), Priority.MEDIA);
-
-
     }
 
-
     @Test
+    @DisplayName("Test List Tasks")
+    @Order(2)
     public void testListTaks() throws Exception {
-        Assert.assertEquals(3, taskManager.listTasks().size());
+
+        assertEquals(3, taskManager.listTasks().size());
     }
 
     @Test
+    @DisplayName("Test Get Task")
+    @Order(1)
     public void testgetTask() throws Exception {
-        Assert.assertEquals("Task Description 1", taskManager.getTask(0).getDescription());
-        Assert.assertEquals(Priority.BAIXA, taskManager.getTask(1).getPriority());
+        assertEquals("Task Description 1", taskManager.getTask(0).getDescription());
+        assertEquals(Priority.BAIXA, taskManager.getTask(1).getPriority());
     }
     @Test
+    @DisplayName("Test Create Task")
+    @Order(0)
     public void testSortByPriority() throws Exception {
+        System.out.println(taskManager.listTasks());
         taskManager.sortByPriority();
 
-        Assert.assertEquals("Test Task 1", taskManager.listTasks().get(0).getTitle());
-        Assert.assertEquals("Test Task 2", taskManager.listTasks().get(1).getTitle());
-        Assert.assertEquals("Test Task 3", taskManager.listTasks().get(2).getTitle());
+        assertAll("Title",
+                () -> assertEquals("Test Task 1", taskManager.listTasks().get(0).getTitle()),
+                () -> assertEquals("Test Task 2", taskManager.listTasks().get(1).getTitle()),
+                () -> assertEquals("Test Task 3", taskManager.listTasks().get(2).getTitle())
+        );
     }
 
 
     @Test
+    @Order(3)
+    @DisplayName("Test Sort By Deadline")
     public void testSortByDeadline() throws Exception {
         taskManager.sortByDeadline();
 
-        Assert.assertEquals("Test Task 1", taskManager.listTasks().get(1).getTitle());
-        Assert.assertEquals("Test Task 2", taskManager.listTasks().get(0).getTitle());
-        Assert.assertEquals("Test Task 3", taskManager.listTasks().get(2).getTitle());
+        assertEquals("Test Task 1", taskManager.listTasks().get(1).getTitle());
+        assertEquals("Test Task 2", taskManager.listTasks().get(0).getTitle());
+        assertEquals("Test Task 3", taskManager.listTasks().get(2).getTitle());
     }
-    @Test
+   
+    @DisplayName("Test Update Task")
+    @Order(4)
+    @RepeatedTest(3)
     public void testUpdateTask() throws Exception {
         taskManager.updateTask(0, "Test Task 1 Updated", "Test Description 1 Updated",  LocalDate.parse("2023-08-28"), Priority.ALTA);
 
-        Assert.assertEquals("Test Task 1 Updated", taskManager.getTask(0).getTitle());
-        Assert.assertEquals("Test Description 1 Updated", taskManager.getTask(0).getDescription());
-        Assert.assertEquals(LocalDate.parse("2023-08-28"), taskManager.getTask(0).getDueDate());
+        assertEquals("Test Task 1 Updated", taskManager.getTask(0).getTitle());
+        assertEquals("Test Description 1 Updated", taskManager.getTask(0).getDescription());
+        assertEquals(LocalDate.parse("2023-08-28"), taskManager.getTask(0).getDueDate());
 
     }
 
 
     @Test
+    @DisplayName("Test Delete Task")
+    @Order(5)
     public void testDeleteTask() throws Exception {
         taskManager.deleteTask(0);
-        Assert.assertEquals(2, taskManager.listTasks().size());
+        assertEquals(2, taskManager.listTasks().size());
     }
 }
